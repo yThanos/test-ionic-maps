bcfdea3a700b19bc7370d19b29a11be07e45488d
Vitor Fraporti <vitorfraporti@hotmail.com>
2023-08-25 11:24:32 -0300

    s

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -34,37 +34,10 @@ export class ExploreContainerComponent {
         zoom: 16
       }
     })
-    let map = new google.maps.Map(document.getElementById("map")!, {
+    let map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
       center: { lat: -29.720026670824154, lng: -53.7175518200379 },
       zoom: 16,
     });
-    let overlay = new google.maps.GroundOverlay(
-      "https://i.imgur.com/AldQ3zS.png",
-      {
-        north: northeast[0],
-        south: southwest[0],
-        east: northeast[1],
-        west: southwest[1],
-      }
-    )
-    //overlay.setMap(map);
-
-
-    let polygon = new google.maps.Polygon({
-      paths: [
-        new google.maps.LatLng(-29.715915580049312, -53.7148139715800),
-        new google.maps.LatLng(-29.71598066995641, -53.715209140119450),
-        new google.maps.LatLng(-29.715436280342924, -53.715328372006354),
-        new google.maps.LatLng(-29.715374148731946, -53.714929796841560),
-        new google.maps.LatLng(-29.715915580049312, -53.71481397158000)
-      ],
-      strokeColor: "#FF0000",
-      strokeOpacity: 0.8,
-      strokeWeight: 2,
-      fillColor: "#FF0000",
-      fillOpacity: 0.35,
-    })
-    //polygon.setMap(map);
 
     for(let poly of this.data.polygons){
       let path = [];
@@ -85,3 +58,37 @@ export class ExploreContainerComponent {
 }
 const northeast = [-29.710317267445962, -53.708645705427465]
 const southwest = [-29.727996585731866, -53.72328774178500]
+
+
+
+const lixo = () => {
+  let overlay = new google.maps.GroundOverlay(
+    "https://i.imgur.com/AldQ3zS.png",
+    {
+      north: northeast[0],
+      south: southwest[0],
+      east: northeast[1],
+      west: southwest[1],
+    }
+  )
+  //overlay.setMap(map);
+
+  navigator.geolocation.getCurrentPosition((position) => {
+  });
+
+  let polygon = new google.maps.Polygon({
+    paths: [
+      new google.maps.LatLng(-29.715915580049312, -53.7148139715800),
+      new google.maps.LatLng(-29.71598066995641, -53.715209140119450),
+      new google.maps.LatLng(-29.715436280342924, -53.715328372006354),
+      new google.maps.LatLng(-29.715374148731946, -53.714929796841560),
+      new google.maps.LatLng(-29.715915580049312, -53.71481397158000)
+    ],
+    strokeColor: "#FF0000",
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+  })
+  //polygon.setMap(map);
+}
